c34e1fed6835e82ecf2a0b820fdc01768d4a498a
Stefan <stephjes17@gmail.com>
2025-09-28 18:45:13 +0200

    Added inputs and Tailwind styles in PatientDetails component

--- app/dashboard/patient/_components/PatientDetails.tsx
+++ app/dashboard/patient/_components/PatientDetails.tsx
@@ -1,9 +1,73 @@
+"use client";
+
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { InputDateSelector } from "@/components/InputDateSelector";
 
 export default function PatientDetails() {
   return (
-    <div>
-      <h2>PatientDetails</h2>
+    <div className="w-[450px]">
+      <div className="inputSection">
+        <div className="inputControl">
+          <label htmlFor="">First Name</label>
+          <input type="text" />
+        </div>
+        <div className="inputControl">
+          <label htmlFor="">Last Name</label>
+          <input type="text" />
+        </div>
+      </div>
+      <div className="inputSection">
+        <div className="inputControl">
+          <label htmlFor="">Gender</label>
+          <Select>
+            <SelectTrigger className="w-full bg-[var(--background)]">
+              <SelectValue
+                placeholder="Select Gender"
+                className="text-[var(--text-soft)]"
+              />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Male</SelectItem>
+              <SelectItem value="dark">Female</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="inputControl">
+          <label htmlFor="">Date of birth</label>
+          <div className="w-full">
+            <InputDateSelector />
+          </div>
+        </div>
+      </div>
+      <div className="inputControl">
+        <label htmlFor="">NationalId (JMBG)</label>
+        <input type="text" />
+      </div>
+      <div className="inputControl">
+        <label htmlFor="">Email</label>
+        <input type="text" />
+      </div>
+      <div className="inputControl">
+        <label htmlFor="">Phone</label>
+        <input type="number" />
+      </div>
+      <div className="inputSection">
+        <div className="inputControl">
+          <label htmlFor="">City</label>
+          <input type="text" />
+        </div>
+        <div className="inputControl">
+          <label htmlFor="">Postal Code</label>
+          <input type="number" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/InputDateSelector.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import * as React from "react";
+import { ChevronDownIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function InputDateSelector() {
+  const [open, setOpen] = React.useState(false);
+  const [date, setDate] = React.useState<Date | undefined>(undefined);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          id="date"
+          className="data-[empty=true]:text-muted-foreground w-full justify-between font-normal text-[var(--text-soft)]"
+        >
+          {date ? date.toLocaleDateString() : "Select date"}
+          <ChevronDownIcon />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          captionLayout="dropdown"
+          onSelect={(date) => {
+            setDate(date);
+            setOpen(false);
+          }}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
