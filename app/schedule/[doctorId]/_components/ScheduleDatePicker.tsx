578de038a10a115ea4978bb41a4d32f88acfd5a6
Stefan <stephjes17@gmail.com>
2025-10-13 22:39:24 +0200

    Working with custom calendar in Schedule component

---
+++ app/schedule/[doctorId]/_components/ScheduleDatePicker.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { useState, useEffect } from "react";
+
+export default function ScheduleDatePicker() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  return (
+    <div>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="my-4 md:max-w-[70%]  lg:max-w-[55%] md:mx-auto w-full"
+        captionLayout={"buttons" as any}
+      />
+    </div>
+  );
+}
--- app/schedule/[doctorId]/page.tsx
+++ app/schedule/[doctorId]/page.tsx
@@ -1,21 +1,31 @@
 import React from "react";
+import ScheduleDatePicker from "./_components/ScheduleDatePicker";
 
 export default function page() {
   return (
-    <div className="container h-screen">
+    <div className="container h-screen ">
       <h1 className="text-lg font-bold py-2">AppDoc</h1>
       <div className="sheduleContainer">
         <div>
-          <h2>Dr Kevin </h2>
-          <h2>Cardiologists</h2>
+          <h2 className="text-lg font-bold">Dr. Kevin Johnson</h2>
+          <h2 className="text-[var(--text-soft)] font-bold -mt-1">
+            Cardiologists
+          </h2>
         </div>
         <div>
-          <h2>Select available time</h2>
+          <h2 className="text-lg font-bold">
+            Browse the available time slots and choose the one that works best
+            for you
+          </h2>
         </div>
         <div className="scheduleMainWrapper">
-          <div className="scheduleMain">
-            <div className="mainLeft"></div>
-            <div className="mainRight"></div>
+          <div className="scheduleMain min-h-[200px]">
+            <div className="flex-1">
+              <ScheduleDatePicker />
+            </div>
+            <div className="mainRight flex-1 border-2 border-amber-300">
+              <h2>SSS</h2>
+            </div>
           </div>
           <div className="inputSection">
             <input type="text" name="" id="" />
