b3f99d8e083b5055d7831d10489d826ef27391e6
Stefan <stephjes17@gmail.com>
2025-11-22 19:08:09 +0100

    Created MainNotification component for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import Navigation from "@/components/Navigation";
+import MainNotification from "@/components/hero/MainNotification";
 
 export default function Home() {
   return (
     <>
+      <MainNotification />
       <Navigation />
       <main>
         <h2>Hello World</h2>
---
+++ components/hero/MainNotification.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { Info, X } from "lucide-react";
+import { useState } from "react";
+
+export default function MainNotification() {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div
+      className={
+        !isOpen
+          ? "hidden"
+          : "bg-[var(--lp-primary)] text-[var(--text)] h-[35px] w-full px-2 flex items-center justify-between"
+      }
+    >
+      <div className="flex items-center gap-1">
+        <Info size={18} />
+        <h1>Send your link, simplify scheduling, and save time!</h1>
+      </div>
+      <div className="actions">
+        <button className="cursor-pointer" onClick={() => setIsOpen(false)}>
+          <X size={18} />
+        </button>
+      </div>
+    </div>
+  );
+}
