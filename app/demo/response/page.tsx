678f6f5992970cb668496a4fa5fad988c5996bf5
Stefan <stephjes17@gmail.com>
2026-02-28 15:22:37 +0100

    Refactor demo response and schedule components

--- app/demo/response/page.tsx
+++ app/demo/response/page.tsx
@@ -6,15 +6,26 @@ import ScheduleFooter from "@/app/schedule/[doctorId]/_components/ScheduleFooter
 import { useDemoSchedule } from "@/store/useDemoSchedule";
 import { formatAppointmentDate } from "@/lib/formatAppointmentDate";
 import { formatAppointmentTime } from "@/lib/formatAppointmentTime";
+import Link from "next/link";
+import { useThemeState } from "@/store/useTheme";
 
 export default function page() {
   const { date } = useDemoSchedule();
+  const { theme } = useThemeState();
   if (!date) {
     throw new Error("Date is required");
   }
   return (
     <div className="container h-screen">
-      <h2>App</h2>
+      <div className="absolute w-[200px] h-[50px] flex items-center top-5 left-2 -ml-9">
+        <Link href={"/"} className="cursor-pointer">
+          <img
+            className="h-full w-auto object-contain"
+            src={theme === "light" ? "/logo.png" : "/logo-light.png"}
+            alt="logo"
+          />
+        </Link>
+      </div>
       <div className="h-[100vh] flex flex-col items-center justify-center">
         <div className="bg-[var(--bg)] p-4 py-6 max-w-[500px] rounded-lg text-center -mt-20">
           <CircleCheckBig
--- app/schedule/[doctorId]/_components/ScheduleMain.tsx
+++ app/schedule/[doctorId]/_components/ScheduleMain.tsx
@@ -9,10 +9,12 @@ import ScheduleDatePicker from "./ScheduleDatePicker";
 import AvailableDateCard from "./AvailableDateCard";
 import ScheduleForm from "./ScheduleForm";
 import ScheduleFooter from "./ScheduleFooter";
-import { SquareUser, GraduationCap } from "lucide-react";
+import { SquareUser } from "lucide-react";
 import { getUser } from "@/app/actions/user";
 import Link from "next/link";
 import { useThemeState } from "@/store/useTheme";
+import Image from "next/image";
+import { Globe, Hospital } from "lucide-react";
 
 interface FreeSlot {
   dayOfWeek: number;
@@ -54,75 +56,168 @@ export default function ScheduleMain({ doctorId }: { doctorId: string }) {
     setActiveIndex(null);
   }, [selectedDate]);
 
+  console.log(doctorData);
+
   return (
-    <div className="container min-h-screen">
-      <div className="relative w-[200px] h-[50px] flex items-center -ml-9">
-        <Link href={"/"} className="cursor-pointer">
-          <img
-            className="h-full w-auto object-contain"
-            src={theme === "light" ? "/logo.png" : "/logo-light.png"}
-            alt="logo"
-          />
-        </Link>
-      </div>
-      <div className="sheduleContainer max-w-[790px] mx-auto -mt-1">
-        {doctorData && !("error" in doctorData) && (
-          <div className="mb-2">
-            <h2 className="text-2xl font-bold flex gap-2 items-center">
-              <SquareUser size={30} />
-              Dr. {`${doctorData?.firstName} ${doctorData?.lastName}`}
-            </h2>
-            <div className="flex gap-2 items-center">
-              <GraduationCap size={30} />
-              <h2 className="text-xl font-bold  text-[var(--btn-primary)] ">
-                {doctorData?.speciality?.trim()
-                  ? doctorData.speciality
-                  : "General Medicine"}
-              </h2>
-            </div>
-          </div>
-        )}
-        <div>
-          <h2 className="text-xl font-bold">
-            Browse the available time slots and choose the one that works best
-            for you
-          </h2>
-        </div>
-        <div className="scheduleMainWrapper bg-[var(--bg)]">
-          <div className="scheduleMain max-h-[420px]">
-            <div className="flex-1">
-              <ScheduleDatePicker
-                selectedDate={selectedDate}
-                setSelectedDate={setSelectedDate}
+    <div className="container">
+      {doctorData && !("error" in doctorData) && (
+        <div className="relative max-w-[950px] mx-auto">
+          <div className="absolute w-[200px] h-[50px] flex items-center top-5 left-2 -ml-9">
+            <Link href={"/"} className="cursor-pointer">
+              <img
+                className="h-full w-auto object-contain"
+                src={theme === "light" ? "/logo.png" : "/logo-light.png"}
+                alt="logo"
               />
+            </Link>
+          </div>
+          <div className="flex gap-4 pt-4">
+            {/* doctor container */}
+            <div className="flex-2 bg-[var(--bg)] rounded-lg px-2">
+              <div className="relative w-[200px] h-[200px] rounded-full border mx-auto mt-12 overflow-hidden mb-2">
+                <Image
+                  src={
+                    doctorData?.profileImage
+                      ? doctorData.profileImage.url
+                      : "/default-profile.png"
+                  }
+                  fill
+                  alt="default_profile_image"
+                />
+              </div>
+              <div className="mb-2 text-center">
+                <h2 className="text-2xl font-bold">
+                  Dr. {`${doctorData?.firstName} ${doctorData?.lastName}`}
+                </h2>
+                <h2 className="text-xl font-bold  text-[var(--btn-primary)] ">
+                  {doctorData?.speciality?.trim()
+                    ? doctorData.speciality
+                    : "General Medicine"}
+                </h2>
+              </div>
+              <div className="line"></div>
+              <ul className="text-lg mt-2">
+                <li className="flex gap-2 items-center my-1">
+                  <SquareUser size={20} />
+                  <p>+5 years of experience</p>
+                </li>
+                <li className="flex gap-2 items-center my-1">
+                  <Hospital size={20} />
+                  <p>St Hospital</p>
+                </li>
+                <li className="flex gap-2 items-center my-1">
+                  <Globe size={20} />
+                  <p>English, German</p>
+                </li>
+              </ul>
             </div>
-            <div className="mainRight flex-1 px-3 overflow-y-scroll h-[420px]">
-              {availableDates?.length ? (
-                availableDates?.map((data, index) => (
-                  <AvailableDateCard
-                    key={index}
-                    dateData={data}
-                    isActive={activeIndex === index}
-                    onClick={(dateData: any) => (
-                      setActiveIndex(index),
-                      setTimeCard(dateData)
-                    )}
+            {/* schedule container */}
+            <div className="scheduleMainWrapper bg-[var(--bg)] flex-5">
+              <div className="scheduleMain max-h-[420px]">
+                <div className="flex-1">
+                  <ScheduleDatePicker
+                    selectedDate={selectedDate}
+                    setSelectedDate={setSelectedDate}
                   />
-                ))
-              ) : (
-                <div className="h-full flex items-center justify-center">
-                  <h2 className="text-xl text-[var(--text-soft)]">
-                    {" "}
-                    Please select another date...
-                  </h2>
                 </div>
-              )}
+                <div className="mainRight flex-1 px-3 overflow-y-scroll h-[330px]">
+                  {availableDates?.length ? (
+                    availableDates?.map((data, index) => (
+                      <AvailableDateCard
+                        key={index}
+                        dateData={data}
+                        isActive={activeIndex === index}
+                        onClick={(dateData: any) => (
+                          setActiveIndex(index),
+                          setTimeCard(dateData)
+                        )}
+                      />
+                    ))
+                  ) : (
+                    <div className="h-full flex items-center justify-center">
+                      <h2 className="text-xl text-center text-[var(--text-soft)]">
+                        {" "}
+                        Please choose a valid date to schedule your appointment
+                      </h2>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <ScheduleForm doctorId={doctorId} selectedTime={timeCard} />
             </div>
           </div>
-          <ScheduleForm doctorId={doctorId} selectedTime={timeCard} />
+          <ScheduleFooter />
         </div>
-        <ScheduleFooter />
-      </div>
+      )}
     </div>
+    // <div className="container min-h-screen">
+    //   <div className="relative w-[200px] h-[50px] flex items-center -ml-9">
+    //     <Link href={"/"} className="cursor-pointer">
+    //       <img
+    //         className="h-full w-auto object-contain"
+    //         src={theme === "light" ? "/logo.png" : "/logo-light.png"}
+    //         alt="logo"
+    //       />
+    //     </Link>
+    //   </div>
+    //   <div className="sheduleContainer max-w-[790px] mx-auto -mt-1">
+    //     {doctorData && !("error" in doctorData) && (
+    //       <div className="mb-2">
+    //         <h2 className="text-2xl font-bold flex gap-2 items-center">
+    //           <SquareUser size={30} />
+    //           Dr. {`${doctorData?.firstName} ${doctorData?.lastName}`}
+    //         </h2>
+    //         <div className="flex gap-2 items-center">
+    //           <GraduationCap size={30} />
+    //           <h2 className="text-xl font-bold  text-[var(--btn-primary)] ">
+    //             {doctorData?.speciality?.trim()
+    //               ? doctorData.speciality
+    //               : "General Medicine"}
+    //           </h2>
+    //         </div>
+    //       </div>
+    //     )}
+    //     <div>
+    //       <h2 className="text-xl font-bold">
+    //         Browse the available time slots and choose the one that works best
+    //         for you
+    //       </h2>
+    //     </div>
+    //     <div className="scheduleMainWrapper bg-[var(--bg)]">
+    //       <div className="scheduleMain max-h-[420px]">
+    //         <div className="flex-1">
+    //           <ScheduleDatePicker
+    //             selectedDate={selectedDate}
+    //             setSelectedDate={setSelectedDate}
+    //           />
+    //         </div>
+    //         <div className="mainRight flex-1 px-3 overflow-y-scroll h-[420px]">
+    //           {availableDates?.length ? (
+    //             availableDates?.map((data, index) => (
+    //               <AvailableDateCard
+    //                 key={index}
+    //                 dateData={data}
+    //                 isActive={activeIndex === index}
+    //                 onClick={(dateData: any) => (
+    //                   setActiveIndex(index),
+    //                   setTimeCard(dateData)
+    //                 )}
+    //               />
+    //             ))
+    //           ) : (
+    //             <div className="h-full flex items-center justify-center">
+    //               <h2 className="text-xl text-[var(--text-soft)]">
+    //                 {" "}
+    //                 Please select another date...
+    //               </h2>
+    //             </div>
+    //           )}
+    //         </div>
+    //       </div>
+    //       <ScheduleForm doctorId={doctorId} selectedTime={timeCard} />
+    //     </div>
+    //     <ScheduleFooter />
+    //   </div>
+    // </div>
   );
 }
